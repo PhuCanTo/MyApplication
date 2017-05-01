ac3d77b0e6f8a7fbb28b6bed151a49531299db07
local student <student@studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2017-05-01 19:55:04 -0400

    Initial commit

---
+++ app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.student.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void AppetizerClick(View view) {
+        Toast.makeText(MainActivity.this, "Fried Shrimps!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void SaladClick(View view) {
+        Toast.makeText(MainActivity.this, "Mixed Vegetables!",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void PastaClick(View view) {
+        Toast.makeText(MainActivity.this, "Vegetarian Lasagna!",
+                Toast.LENGTH_LONG).show();
+        Toast.makeText(MainActivity.this, "Vegetarian Noodle",
+                Toast.LENGTH_LONG).show();
+    }
+
+}
